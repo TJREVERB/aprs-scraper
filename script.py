fc4843273b352c4a1bbb7e741cfeeec250cd7ab7
AkashBhave <akashbbhave@gmail.com>
2018-09-21 16:23:11 -0400

    Add API key and request functionality

--- script.py
+++ script.py
@@ -2,11 +2,20 @@ import requests
 
 
 def main():
-    getData()
+    print(getData())
 
 
 def getData():
-    pass
+    API_KEY = "117144.6TRIzMhji4Kxw"
+    NAME = "CW5773"
+
+    PARAMS = {"name": NAME, "apikey": API_KEY, "what": "loc", "format": "json"}
+    URL = "https://api.aprs.fi/api/get"
+
+    response = requests.get(URL, params=PARAMS)
+
+    return response.content
+
 
 if __name__ == '__main__':
     main()
