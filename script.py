1c7a95d36617c00371dd7de78e867391cc5cad6c
AkashBhave <akashbbhave@gmail.com>
2018-09-21 21:08:50 -0400

    Write data to data.txt

--- script.py
+++ script.py
@@ -2,20 +2,34 @@ import requests
 
 
 def main():
-    print(getData())
+    CALL_SIGN = "ARISS"  # APRS call sign for the ISS
+
+    response = getData(CALL_SIGN)
+    responseJSON = response.json()
+
+    if int(responseJSON["found"]) > 0:  # Ensure valid response that has at least 1 entry
+        print(responseJSON["entries"])
+        writeDataToFile(responseJSON)
+    else:
+        print("No entries found")
 
 
-def getData():
-    API_KEY = "117144.6TRIzMhji4Kxw"
-    NAME = "CW5773"
+def getData(CALL_SIGN):
+    API_KEY = "117144.6TRIzMhji4Kxw"  # API key used to connect to aprs.fi
 
-    PARAMS = {"name": NAME, "apikey": API_KEY, "what": "loc", "format": "json"}
+    PARAMS = {"name": CALL_SIGN, "apikey": API_KEY, "what": "loc", "format": "json"}
     URL = "https://api.aprs.fi/api/get"
 
     response = requests.get(URL, params=PARAMS)
 
-    return response.content
+    return response
 
+
+def writeDataToFile(responseJSON):
+    file = open("data.txt", "a")
+    file.write(str(responseJSON["entries"]))
+    file.write("\n")
+    file.close()
 
 if __name__ == '__main__':
     main()
